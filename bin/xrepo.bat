3b6ce1e106a2b23984614f4d9390b0b5c34be832
Adam Retter <adam.retter@googlemail.com>
2019-05-02 23:11:10 +0400

    Remove files not needed for build

--- bin/xrepo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem # is debug enabled?
-set DEBUG=false
-
-rem # load the common definitions in expath-pkg-common.bat
-call %~dp0/expath-pkg-common.bat
-
-%JAVA% -jar %INSTALL_DIR%/expath/pkg-java.jar %*
